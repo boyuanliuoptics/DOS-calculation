0f57406cac18d0c936e71d29d757414448af826b
boyuanliuoptics <32184481+boyuanliuoptics@users.noreply.github.com>
2017-11-13 16:08:17 +0800

    shell file of edition2

--- dompb.sh
+++ dompb.sh
@@ -6,13 +6,13 @@ file_output_line="band.dat"
 file_output_map_frequency="frequency_GR.txt"
 file_output_map_velocity="velocity_GR.txt"
 # compute frequency band on high symmetry lines
-mpb Zone?=false $file_band_ctl > $data_bandline 
-#mpb-split 8 Zone?=false $file_band_ctl > $data_bandline  # for multithreading, '8' is the number of threads
+#mpb-split 8 Zone?=false $file_band_ctl > $data_bandline
+mpb-split 8 Zone?=false $file_band_ctl > $data_bandline
 grep freqs $data_bandline > $file_output_line
 
 # compute frequency band in the whole Brillouin zone
-mpb Zone?=true $file_band_ctl > $data_bandmap
-#mpb-split 8 Zone?=true $file_band_ctl > $data_bandmap    # for multithreading, '8' is the number of threads
+#mpb Zone?=true $file_band_ctl > $data_bandmap
+mpb-split 8 Zone?=true $file_band_ctl > $data_bandmap
 grep freqs $data_bandmap | tail -n +2 | cut -d ',' -f 7- | cut -c 2- | sed 's/,\ /\n/g' | sed -e 's/$/\r/' > $file_output_map_frequency
 grep velocity $data_bandmap | cut -d ',' -f 3- | cut -c 4-  | sed 's/),\ #(/\n/g' |tr -d ')' | sed -e 's/$/\r/' > $file_output_map_velocity
 
